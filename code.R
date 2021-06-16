2834acf4251712444a546d593ea5c2d568045903
“walid <wasefiani@gmail.com>
2021-06-16 10:58:24 +0200

    premier travail

--- code.R
+++ code.R
@@ -10,6 +10,7 @@ library(readxl)
 library("FactoMineR")
 library("factoextra")
 library(ggplot2)
+library(dplyr)
 
 #données
 dataset_synthese <- read_ods("data.ods",skip=1,sheet="Synthese",col_names=TRUE)
@@ -20,6 +21,7 @@ head(dataset_synthese)
 
 #suppression des colonnes inutiles
 dataset_synthese1 <- dataset_synthese[,c(-1,-2,-6,-7,-8)]
+
 
 #faire une ACP sur les différentes émissions de gaz à effet de serres
 outputs <- dataset_synthese1[,9:22]
@@ -40,18 +42,63 @@ cor(dataset_synthese1$`Score unique EF (mPt/kg de produit)` , axes$axe4, method 
 
 #on garde les 3 derniers axes seulement
 newdataset_synthese <- data.frame(dataset_synthese1[,1:8],axes[,2:4])
-colnames(newdataset_synthese)<-c("groupe_aliment","sgroupe_aliment","produit","livraison","emballage","preparation","note_qualite","score_unique","axe1","axe2","axe3","axe4")
+colnames(newdataset_synthese)<-c("groupe_aliment","sgroupe_aliment","produit","livraison","emballage","preparation","note_qualite","score_unique","axe2","axe3","axe4")
+
+#stat des de la variab§les score_unique
+summary(newdataset_synthese["score_unique"])
+boxplot(newdataset_synthese["score_unique"])
+hist(newdataset_synthese$score_unique)
+porc_out <- length(newdataset_synthese$score_unique[newdataset_synthese$score_unique>2])/2480
+porc_out
+# ils représentent 8% de l'effectif total, on les supprime 
+newdataset_synthese <- droplevels(newdataset_synthese[-which(newdataset_synthese$score_unique > 2), ] )
+
+shapiro.test(newdataset_synthese$score_unique)
+
+#centrer et réduire la variable cible pour s'approcher plus d'une loi normale et valider les hypothèses du modèle inéaire 
+newdataset_synthese["score_unique"] <- scale(newdataset_synthese$score_unique)
 
 
 
-#régression linéaire
-modele<-lm(formula = score_unique ~  note_qualite + groupe_aliment + livraison + emballage + preparation  , data=newdataset_synthese )
-summary(modele)
+#analyse bivariée
+tab <- table(newdataset_synthese$groupe_aliment,newdataset_synthese$livraison)
+tab1 <- table(newdataset_synthese$groupe_aliment,newdataset_synthese$emballage)
+tab2 <- table(newdataset_synthese$groupe_aliment,newdataset_synthese$preparation)
+tab3 <- table(newdataset_synthese$livraison,newdataset_synthese$emballage)
+tab4 <- table(newdataset_synthese$livraison,newdataset_synthese$preparation)
+tab5 <- table(newdataset_synthese$emballage,newdataset_synthese$preparation)
+
+fisher.test(tab,simulate.p.value=TRUE)
+chisq.test(tab1)
+chisq.test(tab2)
+chisq.test(tab3)
+chisq.test(tab4)
+chisq.test(tab5)
+#on a bun certaines une corrélation entre les variables explicatives
+
+print(levels(as.factor(newdataset_synthese$groupe_aliment)))
+print(levels(as.factor(newdataset_synthese$livraison)))
+print(levels(as.factor(newdataset_synthese$emballage)))
+print(levels(as.factor(newdataset_synthese$preparation)))
+
+
+#régression avec modèle linéaire multiple
+modele1<-lm(formula = score_unique ~  note_qualite + groupe_aliment + livraison + emballage + preparation  , data=newdataset_synthese )
+summary(modele1)
+plot(modele1,2)
 #la proba de se tromper en rejetant H0 au niveau 5% est très faible,
 #il y'a donc effet de la qualité des données sur le score unique
 #effet des variables qualitatives:
-drop1(modele,.~.,test="F")
-#on a bien un effet de toutes les variables qualitatives sur le score unique
+drop1(modele1,.~.,test="F")
+
+modele2<-lm(formula = score_unique ~ note_qualite + groupe_aliment + livraison + emballage + preparation + groupe_aliment:note_qualite  + emballage:note_qualite + preparation:note_qualite  , data=newdataset_synthese )
+summary(modele2)
+drop1(modele2,.~.,test="F")
+
+anova(modele1,modele2)
+#on rejette l'hypothèse de non-intéraction entre note qualité et les autres var quali
+#il y' bien un effet des intéractions de la  qualité de l'information sur les variabes quali sur l'mission de gaz
+
 
 #données 2
 ##émission des gaz à effet de serre pour chaque étape du cycle de vie
@@ -61,52 +108,48 @@ sum(is.na(dataset_par_etape))
 colMeans(is.na(dataset_par_etape))
 head(dataset_par_etape)
 
-#,dataset_par_etape[,10],dataset_par_etape[,11],dataset_par_etape[,12],dataset_par_etape[,13],dataset_par_etape[,14]
-#ajout de colonnes
+#ajout de la colonne étape
 colnames(dataset_par_etape)[8:14]<- "score_unique"
-newdataset_par_etape <- rbind(as.data.frame(dataset_par_etape[,8]), as.data.frame(dataset_par_etape[,9]))
 etape <- rbind(rep("agriculture",2480), rep("transformation",2480),rep("emballage",2480),rep("transport",2480),rep("distribution",2480),rep("consommation",2480),rep("total",2480))
 
-a<-cbind(as.data.frame(dataset_par_etape[,8]),rep("agriculture",2480))
-colnames(a) <- c("score_unique","etape")
-b<-cbind(as.data.frame(dataset_par_etape[,9]),rep("transformation",2480))
-colnames(b) <- c("score_unique","etape")
-c<-cbind(as.data.frame(dataset_par_etape[,10]),rep("emballage",2480))
-colnames(c) <- c("score_unique","etape")
-d<-cbind(as.data.frame(dataset_par_etape[,11]),rep("transport",2480))
-colnames(d) <- c("score_unique","etape")
-e<-cbind(as.data.frame(dataset_par_etape[,12]),rep("distribution",2480))
-colnames(e) <- c("score_unique","etape")
-f<-cbind(as.data.frame(dataset_par_etape[,13]),rep("consommation",2480))
-colnames(f) <- c("score_unique","etape")
-g<-cbind(as.data.frame(dataset_par_etape[,14]),rep("total",2480))
-colnames(g) <- c("score_unique","etape")
+a<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,8]),rep("agriculture",2480))
+colnames(a) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
+b<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,9]),rep("transformation",2480))
+colnames(b) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
+c<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,10]),rep("emballage",2480))
+colnames(c) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
+d<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,11]),rep("transport",2480))
+colnames(d) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
+e<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,12]),rep("distribution",2480))
+colnames(e) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
+f<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,13]),rep("consommation",2480))
+colnames(f) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
+g<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,14]),rep("total",2480))
+colnames(g) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
 
 newdataset_par_etape <- rbind(a,b,c,d,e,f,g)
 
+newdataset_par_etape <- droplevels(newdataset_par_etape[-which(newdataset_par_etape$score_unique > 2), ] )
+
+
+#centrer et réduire la variable cible pour s'approcher plus d'une loi normale et valider les hypothèses du modèle inéaire 
+newdataset_synthese["score_unique"] <- scale(newdataset_synthese$score_unique)
+
+
+
 #regression linéaire
-modele1<-lm(formula = score_unique ~  etape,data=newdataset_par_etape)
+modele1<-lm(formula = score_unique ~  note_qualite + groupe_aliment + etape,data=newdataset_par_etape)
 summary(modele1)
-#c'est l'agriculture qui émet plus de gaz à effets de serre
+#c'est l'agriculture qui émet plus de gaz à effets de serre cart tous les coefficients sont négtifs
 #par contre le R2 de la régresion linéaire est faibles pour les 2 modèles, il fut essayer alors d'autres modèles
 
+modele2<-lm(formula = score_unique ~  note_qualite + groupe_aliment + etape + note_qualite*groupe_aliment*etape ,data=newdataset_par_etape)
+summary(modele2)
+drop1(modele2,.~.,test="F")
 
-#suppression des colonnes inutiles
-dataset_par_etape1 <- dataset_par_etape[,c(-1,-2,-6,-7,-8)]
-
-#
-rbind(data frame A, data frame B)
-
-#renommer les colonnes
-colnames(newdataset_synthese)<-c("groupe_aliment","sgroupe_aliment","produit","livraison","emballage","preparation","note_qualite","score_unique","axe1","axe2","axe3","axe4")
-
-
-
+anova(modele1,modele2)
 
 #données 3
 dataset_par_ingredient <- read_ods("data.ods",skip=3,sheet="Detail ingredient",col_names=TRUE)
-
-
-
 data_region <- read_excel("data_region.xlsx")
 
