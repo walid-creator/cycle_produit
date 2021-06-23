16f77b1c02973a588c8a97f7e3891643a907de52
“walid <wasefiani@gmail.com>
2021-06-23 17:55:23 +0200

    stat descriptive

--- code.R
+++ code.R
@@ -3,6 +3,7 @@ setwd("~/dcma")
 #install.packages('readODS')
 #install.packages("tidyverse")
 #install.packages(c("FactoMineR", "factoextra"))
+#install.packages("quantreg")
 
 #libraries
 library(readODS)
@@ -21,6 +22,100 @@ head(dataset_synthese)
 
 #suppression des colonnes inutiles
 dataset_synthese1 <- dataset_synthese[,c(-1,-2,-6,-7,-8)]
+colnames(dataset_synthese1)<-c("groupe_aliment","sgroupe_aliment","produit","livraison","emballage","preparation","note_qualite","score_unique", "CO2","appauvrissement","rayonnement","Formation","Particules","Acidifaction","EutrophisationT","EutrophisationTEau","EutrophisationMarine","UtilisationSol","Ecotoxicité","EpuisementEAu","EpuisementEnergie","EpuisementMinéraux")
+
+#analyse desc de la variable d'intérêt 
+#tab <- dataset_synthese1[, c(1)] 
+#tab_disj <- tab.disjonctif(tab)
+summary(dataset_synthese1['note_qualite'])
+hist(dataset_synthese1$note_qualite)
+#modalité de régérence
+dataset_synthese1$groupe_aliment <- relevel(factor(dataset_synthese1$groupe_aliment), "entrées et plats composés")
+
+#l qualité de l'info pour chaque groupe d'aliment
+reg<-lm(formula = note_qualite ~    groupe_aliment, data = dataset_synthese1)
+summary(reg)
+drop1(reg,.~.,test="F")
+plat_comp <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'entrées et plats composés' ),] 
+glaces <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'glaces et sorbets' ),] 
+grasses <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'matières grasses' ),] 
+sucres <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'produits sucrés' ),] 
+culinaires <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'aides culinaires et ingrédients divers' ),] 
+boissons <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'boissons' ),] 
+viandes <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'viandes, œufs, poissons' ),] 
+fruits_leg <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'fruits, légumes, légumineuses et oléagineux' ),] 
+infantiles <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'aliments infantiles' ),] 
+céréaliers <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'produits céréaliers' ),] 
+laitiers <- dataset_synthese1[which(dataset_synthese1$groupe_aliment == 'lait et produits laitiers' ),] 
+
+
+#hétérogeinéité au sein de chaque groupe d'aliment
+mean(plat_comp$note_qualite)#
+mean(glaces$note_qualite)#
+mean(grasses$note_qualite)#
+mean(sucres$note_qualite)#
+mean(culinaires$note_qualite)#
+mean(boissons$note_qualite)#
+mean(viandes$note_qualite)#
+mean(fruits_leg$note_qualite)#
+mean(infantiles$note_qualite)#
+mean(céréaliers$note_qualite)#
+mean(laitiers$note_qualite)#
+#aucun sous échantillon 
+x<-dataset_synthese1$groupe_aliment
+y<-dataset_synthese1$score_unique
+var.test(x,y)
+?aov
+fit <- aov(score_unique ~ groupe_aliment, data=dataset_synthese1 )
+summary(fit)
+#on a une pvalue  significative, on rejette l'hypothèse d'égalité des moyennes,
+#il existe au moins une paire de moyenne qui ne sont pas égales entre 2 groupes
+
+
+#normalité
+library("car")
+qqPlot(newdataset_synthese$score_unique)
+
+#Boxplots non réussi
+boxplot(plat_comp['note_qualite'],col="#FAD4B1") ; 
+split.screen(1:11)
+plot.new() 
+par(mar=c(1,1,1,0)) 
+boxplot(plat_comp['note_qualite'],col="#FAD4B1")
+axis(2) 
+par(new = T) 
+par(mar=c(1,0,1,5)) 
+boxplot(grasses['note_qualite'],col="#FAD4B1")
+axis(4) 
+boxplot(plat_comp['note_qualite'],col="#FAD4B1")
+boxplot(glaces['note_qualite'],col="#FAD4B1")
+boxplot(grasses['note_qualite'],col="#FAD4B1")
+boxplot(sucres['note_qualite'],col="#FAD4B1")
+boxplot(culinaires['note_qualite'],col="#FAD4B1")
+boxplot(boissons['note_qualite'],col="#FAD4B1")
+boxplot(viandes['note_qualite'],col="#FAD4B1")
+boxplot(fruits_leg['note_qualite'],col="#FAD4B1")
+boxplot(infantiles['note_qualite'],col="#FAD4B1")
+boxplot(céréaliers['note_qualite'],col="#FAD4B1")
+boxplot(litiers['note_qualite'],col="#FAD4B1")
+
+
+
+
+#pourcentage des aliments en bonne qualité pour chaque aliment:
+pourc1<-length(plat_comp[which(plat_comp$note_qualite<=3 ),1])/length(plat_comp[,1])
+pourc2<-length(glaces[which(glaces$note_qualite<=3 ),1])/length(glaces[,1])
+pourc3<-length(grasses[which(grasses$note_qualite<=3 ),1])/length(grasses[,1])
+pourc4<-length(sucres[which(sucres$note_qualite<=3 ),1])/length(sucres[,1])
+pourc5<-length(culinaires[which(culinaires$note_qualite<=3 ),1])/length(culinaires[,1])
+pourc6<-length(boissons[which(boissons$note_qualite<=3 ),1])/length(boissons[,1])
+pourc7<-length(viandes[which(viandes$note_qualite<=3 ),1])/length(viandes[,1])
+pourc8<-length(fruits_leg[which(fruits_leg$note_qualite<=3 ),1])/length(fruits_leg[,1])
+pourc9<-length(infantiles[which(infantiles$note_qualite<=3 ),1])/length(infantiles[,1])
+pourc10<-length(céréaliers[which(céréaliers$note_qualite<=3 ),1])/length(céréaliers[,1])
+pourc11<-length(laitiers[which(laitiers$note_qualite<=3 ),1])/length(laitiers[,1])
+pourc_total<-length(dataset_synthese1[which(dataset_synthese1$note_qualite<=3 ),1])/length(dataset_synthese1[,1])
+
 
 
 #faire une ACP sur les différentes émissions de gaz à effet de serres
@@ -28,21 +123,55 @@ outputs <- dataset_synthese1[,9:22]
 acp<-PCA(outputs, scale.unit = TRUE, graph = TRUE)
 print(acp)
 print(acp$eig)
+
+#contribution d'une variable  à un axe
+#on garde les individus qui ont une contribution supérieure à la contribution
+#moyenne 1/n
+acp$var$contrib
+contr_ind<-data.frame(acp$ind$contrib,newdataset_synthese["groupe_aliment"],newdataset_synthese["sgroupe_aliment"],newdataset_synthese["produit"])
+contr_ind_axe1 <- contr_ind[which(contr_ind$Dim.1>1/2480),c(1,6,7,8)]
+contr_ind_axe2 <- contr_ind[which(contr_ind$Dim.2>1/2480),c(2,6,7,8)]
+contr_ind_axe3 <- contr_ind[which(contr_ind$Dim.3>1/2480),c(3,6,7,8)]
+contr_ind_axe4 <- contr_ind[which(contr_ind$Dim.4>1/2480),c(4,6,7,8)]
+contr_ind_axe5 <- contr_ind[which(contr_ind$Dim.5>1/2480),c(5,6,7,8)]
+
+x<-data.frame(table(contr_ind$groupe_aliment))
+
+x1<-data.frame(table(contr_ind_axe1$groupe_aliment))
+freqAxe1 <- data.frame(x1['Var1'],x1['Freq']/x['Freq'])
+x2<-data.frame(table(contr_ind_axe1$groupe_aliment))
+freqAxe2 <- data.frame(x2['Var1'],x2['Freq']/x['Freq'])
+x3<-data.frame(table(contr_ind_axe3$groupe_aliment))
+freqAxe3 <- data.frame(x3['Var1'],x3['Freq']/x['Freq'])
+x4<-data.frame(table(contr_ind_axe1$groupe_aliment))
+freqAxe4 <- data.frame(x4['Var1'],x4['Freq']/x['Freq'])
+x5<-data.frame(table(contr_ind_axe1$groupe_aliment))
+freqAxe5 <- data.frame(x5['Var1'],x5['Freq']/x['Freq'])
+
+#fréquence de chaque groupe d'aliment bien présenté dans chaque axe
+tot<-data.frame(x1['Var1'],freqAxe1['Freq'],freqAxe2['Freq'],freqAxe3['Freq'],freqAxe4['Freq'],freqAxe5['Freq'])
+colnames(tot)<-c("groupe produit","axe1","axe2","axe3","axe4","axe5")
+
+acp$var$cos2
 axes<-acp$ind$coord
 #d'après la règle de kaiser on retient 4 axes (vp>1)
-axes<-axes[,-5]
-colnames(axes) <- c("axe1","axe2","axe3","axe4")
+#Appauvrissement de la couche d'Ozone est complètement représentée par le cinquième axe
+
+colnames(axes) <- c("axe1","axe2","axe3","axe4","axe5")
 axes <- as.data.frame(axes)
 
-#corrélation entre le Score unique EF et l'émission des autres gaezs à effet de serre
-cor(dataset_synthese1$`Score unique EF (mPt/kg de produit)` , axes$axe1, method = c("pearson", "kendall", "spearman"))#0.99
-cor(dataset_synthese1$`Score unique EF (mPt/kg de produit)` , axes$axe2, method = c("pearson", "kendall", "spearman"))#0.01
-cor(dataset_synthese1$`Score unique EF (mPt/kg de produit)` , axes$axe3, method = c("pearson", "kendall", "spearman"))#0.11
-cor(dataset_synthese1$`Score unique EF (mPt/kg de produit)` , axes$axe4, method = c("pearson", "kendall", "spearman"))#0.03
 
-#on garde les 3 derniers axes seulement
-newdataset_synthese <- data.frame(dataset_synthese1[,1:8],axes[,2:4])
-colnames(newdataset_synthese)<-c("groupe_aliment","sgroupe_aliment","produit","livraison","emballage","preparation","note_qualite","score_unique","axe2","axe3","axe4")
+
+#corrélation entre le Score unique EF et l'émission des autres gaezs à effet de serre
+cor(dataset_synthese1$score_unique , axes$axe1, method = c("pearson", "kendall", "spearman"))#0.99
+cor(dataset_synthese1$score_unique , axes$axe2, method = c("pearson", "kendall", "spearman"))#0.01
+cor(dataset_synthese1$score_unique , axes$axe3, method = c("pearson", "kendall", "spearman"))#0.11
+cor(dataset_synthese1$score_unique , axes$axe4, method = c("pearson", "kendall", "spearman"))#0.03
+cor(dataset_synthese1$score_unique , axes$axe5, method = c("pearson", "kendall", "spearman"))#0.03
+
+#on garde les 4 derniers axes seulement
+newdataset_synthese <- data.frame(dataset_synthese1[,1:8],axes[,2:5])
+colnames(newdataset_synthese)<-c("groupe_aliment","sgroupe_aliment","produit","livraison","emballage","preparation","note_qualite","score_unique","axe2","axe3","axe4","axe5")
 
 #stat des de la variab§les score_unique
 summary(newdataset_synthese["score_unique"])
@@ -51,8 +180,9 @@ hist(newdataset_synthese$score_unique)
 porc_out <- length(newdataset_synthese$score_unique[newdataset_synthese$score_unique>2])/2480
 porc_out
 # ils représentent 8% de l'effectif total, on les supprime 
-newdataset_synthese <- droplevels(newdataset_synthese[-which(newdataset_synthese$score_unique > 2), ] )
+#newdataset_synthese <- droplevels(newdataset_synthese[-which(newdataset_synthese$score_unique > 2), ] )
 
+#test de normlité
 shapiro.test(newdataset_synthese$score_unique)
 
 #centrer et réduire la variable cible pour s'approcher plus d'une loi normale et valider les hypothèses du modèle inéaire 
@@ -74,13 +204,55 @@ chisq.test(tab2)
 chisq.test(tab3)
 chisq.test(tab4)
 chisq.test(tab5)
-#on a bun certaines une corrélation entre les variables explicatives
+#on a bien certaines une corrélation entre les variables explicatives
 
+#modalité des variables
 print(levels(as.factor(newdataset_synthese$groupe_aliment)))
 print(levels(as.factor(newdataset_synthese$livraison)))
 print(levels(as.factor(newdataset_synthese$emballage)))
 print(levels(as.factor(newdataset_synthese$preparation)))
 
+#pourcentage de chaque températures 
+print(length(newdataset_synthese$livraison[newdataset_synthese$livraison == "Congelé"])/length(newdataset_synthese$livraison))
+print(length(newdataset_synthese$livraison[newdataset_synthese$livraison == "Glacé"])/length(newdataset_synthese$livraison))
+print(length(newdataset_synthese$livraison[newdataset_synthese$livraison == "Ambiant (court)"])/length(newdataset_synthese$livraison))
+print(length(newdataset_synthese$livraison[newdataset_synthese$livraison == "Ambiant (long)"])/length(newdataset_synthese$livraison))
+print(length(newdataset_synthese$livraison[newdataset_synthese$livraison == "Ambiant (moyenne)"])/length(newdataset_synthese$livraison))
+
+#pourcentage de chaque type d'emballlage
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Acier"])/length(newdataset_synthese$emballage))#0.04
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Aluminium"])/length(newdataset_synthese$emballage))#0.0004
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Bouteille PET"])/length(newdataset_synthese$emballage))#0.0004
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Bouteille PETE"])/length(newdataset_synthese$emballage))#0.027
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Carton"])/length(newdataset_synthese$emballage))#0.12
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Déjà emballé - Aluminium"])/length(newdataset_synthese$emballage))#0.006
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Déjà emballé - LDPE"])/length(newdataset_synthese$emballage))#0.0028
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Déjà emballé - PET"])/length(newdataset_synthese$emballage))#0.0625
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Déjà emballé - PP/PE"])/length(newdataset_synthese$emballage))#0.01
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Déjà emballé - Verre"])/length(newdataset_synthese$emballage))#0.0056
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "HPDE"])/length(newdataset_synthese$emballage))#0.0125
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "LPDE"])/length(newdataset_synthese$emballage))#0.19
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Papier"])/length(newdataset_synthese$emballage))#0.014
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Pas d'emballage"])/length(newdataset_synthese$emballage))#0.063
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "PP"])/length(newdataset_synthese$emballage))#0.156
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "PS"])/length(newdataset_synthese$emballage))#0.208
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "PVC"])/length(newdataset_synthese$emballage))#0.0004
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "V (PVC)"])/length(newdataset_synthese$emballage))#0.0326
+print(length(newdataset_synthese$emballage[newdataset_synthese$emballage == "Verre"])/length(newdataset_synthese$emballage))#0.037
+
+#pourcentage de chaque type de preparation
+print(length(newdataset_synthese$preparation[newdataset_synthese$preparation == "Bouilli"])/length(newdataset_synthese$preparation))#0.013
+print(length(newdataset_synthese$preparation[newdataset_synthese$preparation == "Cuisson à l'eau"])/length(newdataset_synthese$preparation))#0.031
+print(length(newdataset_synthese$preparation[newdataset_synthese$preparation == "Four"])/length(newdataset_synthese$preparation))#0.122
+print(length(newdataset_synthese$preparation[newdataset_synthese$preparation == "Friture"])/length(newdataset_synthese$preparation))#0.0004
+print(length(newdataset_synthese$preparation[newdataset_synthese$preparation == "Micro onde"])/length(newdataset_synthese$preparation))#0.11
+print(length(newdataset_synthese$preparation[newdataset_synthese$preparation == "Pas de préparation"])/length(newdataset_synthese$preparation))#0.68
+print(length(newdataset_synthese$preparation[newdataset_synthese$preparation == "Poêle"])/length(newdataset_synthese$preparation))#0.015
+print(length(newdataset_synthese$preparation[newdataset_synthese$preparation == "Réfrigéré chez le consommateur"])/length(newdataset_synthese$preparation))#0.023
+
+score_unique_microOnde<-dataset_synthese1[which(dataset_synthese1$preparation=="Micro onde"),c(1,3,8)]
+summary(score_unique_microOnde)
+#régressions
 
 #régression avec modèle linéaire multiple
 modele1<-lm(formula = score_unique ~  note_qualite + groupe_aliment + livraison + emballage + preparation  , data=newdataset_synthese )
@@ -98,6 +270,20 @@ drop1(modele2,.~.,test="F")
 anova(modele1,modele2)
 #on rejette l'hypothèse de non-intéraction entre note qualité et les autres var quali
 #il y' bien un effet des intéractions de la  qualité de l'information sur les variabes quali sur l'mission de gaz
+
+#réression quantile
+library(quantreg)
+?rq
+help.start()
+# un exemple
+example(rq)
+rqfit <- rq(score_unique ~ note_qualite + groupe_aliment + livraison + emballage + preparation  ,tau=c(.05, .25, .5, .75, .95), data=newdataset_synthese)
+summary(rqfit,se = "boot")
+plot(note_qualite ~ groupe_aliment, data = dataset_synthese1, pch = 16)
+abline(lm(note_qualite ~ groupe_aliment, data = dataset_synthese1), col = "red", lty = 2)
+abline(rq(note_qualite ~ groupe_aliment, data = dataset_synthese1), col = "blue", lty = 2)
+legend("topright", legend = c("lm", "rq"), col = c("red", "blue"), lty = 2)
+
 
 
 #données 2
@@ -124,10 +310,9 @@ e<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par
 colnames(e) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
 f<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,13]),rep("consommation",2480))
 colnames(f) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
-g<-cbind(as.data.frame(dataset_par_etape[,c(3,4,5,7)]),as.data.frame(dataset_par_etape[,14]),rep("total",2480))
-colnames(g) <- c("groupe_aliment","sgroupe_aliment","produit","note_qualite","score_unique","etape")
 
-newdataset_par_etape <- rbind(a,b,c,d,e,f,g)
+
+newdataset_par_etape <- rbind(a,b,c,d,e,f)
 
 newdataset_par_etape <- droplevels(newdataset_par_etape[-which(newdataset_par_etape$score_unique > 2), ] )
 
@@ -138,12 +323,12 @@ newdataset_synthese["score_unique"] <- scale(newdataset_synthese$score_unique)
 
 
 #regression linéaire
-modele1<-lm(formula = score_unique ~  note_qualite + groupe_aliment + etape,data=newdataset_par_etape)
+modele1<-lm(formula = score_unique ~  groupe_aliment + etape,data=newdataset_par_etape)
 summary(modele1)
 #c'est l'agriculture qui émet plus de gaz à effets de serre cart tous les coefficients sont négtifs
 #par contre le R2 de la régresion linéaire est faibles pour les 2 modèles, il fut essayer alors d'autres modèles
 
-modele2<-lm(formula = score_unique ~  note_qualite + groupe_aliment + etape + note_qualite*groupe_aliment*etape ,data=newdataset_par_etape)
+modele2<-lm(formula = score_unique ~    groupe_aliment + etape + groupe_aliment:etape + groupe_aliment:etape:note_qualite,data=newdataset_par_etape)
 summary(modele2)
 drop1(modele2,.~.,test="F")
 
